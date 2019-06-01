30f4e69bf9a8aef425fc2554919187052e95d466
shubham-shinde <shindes0321@gmail.com>
2019-06-01 11:04:02 +0530

    Created new project no ganache for this contract and deployed it also, Updated index.js and index.html to connect with contract, Changed truffle-config and set default gas price in it, In migrations deploy contract script is change to match the current voting contract, Before deploying contract array of candidates is convered to Hex i.e. bytes32,

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
+const Voting = artifacts.require("Voting");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(Voting, ['shubham', 'kamran', 'vinayak'].map(x => web3.utils.asciiToHex(x)), {gas: 6000000});
 };
--- truffle-config.js
+++ truffle-config.js
@@ -36,6 +36,12 @@ module.exports = {
    */
 
   networks: {
+    ganache: {
+      host: 'localhost',
+      port: 7545,
+      network_id: '*',
+      gas: 4700000
+    }
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
