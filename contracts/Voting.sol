c0252c322ecf55574f2ad591265280b5b3d09730
shubham-shinde <shindes0321@gmail.com>
2019-06-01 13:12:37 +0530

    struct to save voters info created and mapped to address of the voter In constractor total no. of tokens and token price is included.

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -6,21 +6,34 @@ contract Voting {
   The key of the mapping is candidate name stored as type bytes32 and value is
   an unsigned integer to store the vote count
   */
+  //struct to store the voter information.
+  struct voter {
+    address voterAddress;
+    uint tokensBought;
+    uint[] tokensUsedPerCandidate;
+  }
   
   mapping (bytes32 => uint256) public votesReceived;
+  mapping (address => voter) public voterInfo;
   
   /* Solidity doesn't let you pass in an array of strings in the constructor (yet).
   We will use an array of bytes32 instead to store the list of candidates
   */
   
   bytes32[] public candidateList;
+  uint public totalTokens; // Total no. of tokens available for this election
+  uint public balanceTokens; // Total no. of tokens still available for purchase
+  uint public tokenPrice; // Price of token
 
   /* This is the constructor which will be called once when you
   deploy the contract to the blockchain. When we deploy the contract,
-  we will pass an array of candidates who will be contesting in the election
+  we will pass total no. of tokens, token price and an array of candidates who will be contesting in the election
   */
-  constructor(bytes32[] memory candidateNames) public {
+  constructor(uint tokens, uint pricePerToken, bytes32[] memory candidateNames) public {
     candidateList = candidateNames;
+    totalTokens = tokens;
+    balanceTokens = tokens;
+    tokenPrice = pricePerToken;
   }
 
   // This function returns the total votes a candidate has received so far
